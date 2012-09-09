d8118fee8053052312e32e95a4bb996b45eb0fcd
Adrian Artiles <Adrian.R.Artiles@gmail.com>
2012-09-09 18:11:00 -0500

    small improvements to the twitter and instagram widgets

--- source/javascripts/custom.js
+++ source/javascripts/custom.js
@@ -1,6 +1,14 @@
 $(function(){
   $('[rel="tooltip"]').tooltip();
 
+  if ( window.location.pathname == '/' ) {
+    $('[rel="tooltip"]').hover(
+      function () {
+        $('.tooltip.top').addClass("home");
+      }
+    );
+  }
+
   $(".tweet").tweet({
     username: 'adrianartiles',
     join_text: "auto",
@@ -17,6 +25,7 @@ $(function(){
 
   $(".instagram").instagram({
     userId: 145937522,
+    //hash: 'love',
     clientId: 'c038b3d436624c2893e3623b18354c43',
     accessToken: '145937522.c038b3d.e1709bd3843046f8bc67f45318920313',
     show: 4,
@@ -33,9 +42,13 @@ $(function(){
     if (button.text() != 'Loading…') {
       button.text('Loading…');
       insta_container.instagram({
+          userId: 145937522,
+          clientId: 'c038b3d436624c2893e3623b18354c43',
+          accessToken: '145937522.c038b3d.e1709bd3843046f8bc67f45318920313',
           next_url : insta_next_url,
           show : 4,
           onComplete : function(photos, data) {
+            console.log(data);
             insta_next_url = data.pagination.next_url;
             button.text(text);
             button.removeClass('disabled');
